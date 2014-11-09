f941e5da256ede84d55e70a3e60ba05249831bc7
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-11-09 10:48:37 +0100

    adds route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  post 'posts/:id/upvote' => 'posts#upvote', as: 'upvote'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
